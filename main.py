072140a6c51f1b9a5cb6b3bfbe31dc77db48142c
RedFurrFox <redfurryfoxgaming@gmail.com>
2022-08-24 16:42:23 +0800

    adding finishing touches and patches

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ Settings_Template = """Settings:
     - https://google.com
     - https://facebook.com
     - https://github.com
+    - https://msn.com   
+    - # Feel free to add more site here
 
   Default_Value:
     Timeout: 5
@@ -58,31 +60,36 @@ def Terminal_Cleaner():
     else:
         os.system("cls")
 
-def Ping(url, timeout):
-    global reqg
+def Ping(purl, ptimeout):
+
+    global reqg1
+
     try:
-        reqg = requests.get(url, timeout)
-        print(f'[Console][Requests] Target Url: "{url}" || Status Code: "{reqg.status_code}" || ✔ [Requests Sent :: You Have Internet Access]')
+        reqg1 = requests.get(url=purl, timeout=ptimeout)
+        print(f'[Console][Requests] Target Url: "{purl}" || Status Code: "{reqg1.status_code}" || ✔ [Requests Sent :: You Have Internet Access]')
         pass
     except requests.ReadTimeout:
-        print(f'[Console][Requests] Target Url: "{url}" || Status Code: "{reqg.status_code}" || ✖ [Requests Blocked :: Auth/Server/DDoS Issue]')
+        print(f'[Console][Requests] Target Url: "{purl}" || Status Code: "{reqg1.status_code}" || ✖ [Requests Blocked :: Auth/Server/DDoS Issue]')
         pass
     except (requests.ConnectTimeout, requests.ConnectionError):
-        print(f'[Console][Requests] Target Url: "{url}" || Status Code: "{reqg.status_code}" || ✖ [Requests Not Sent :: No Internet]')
+        print(f'[Console][Requests] Target Url: "{purl}" || Status Code: "{reqg1.status_code}" || ✖ [Requests Not Sent :: No Internet]')
         pass
     except requests.RequestException:
-        print(f'[Console][Requests] Invalid URl => "{url}"')
+        print(f'[Console][Requests] Invalid URl => "{purl}"')
         pass
 
-def Check_Tool_Version(url, timeout, version):
-    global reqg
+def Check_Tool_Version(ctvurl, ctvtimeout, ctvversion):
+    global reqg2
+
     try:
-        requests.get(url="https://camo.githubusercontent.com/12df27140682753703f98aa63ea1afe69472864e061805883667dc107d86efcb/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d52656446757272466f78", timeout=timeout)
-        reqg = requests.get(url, timeout).text
-        if reqg == version:
+        requests.get(url="https://camo.githubusercontent.com/12df27140682753703f98aa63ea1afe69472864e061805883667dc107d86efcb/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d52656446757272466f78", timeout=ctvtimeout)
+        reqg2 = requests.get(url=ctvurl, timeout=ctvtimeout).text
+
+        if reqg2 == ctvversion:
             pass
         else:
-            print('[Console][Warning] Outdated Tool\n')
+            print('[Console][Tip] Outdated Tool... Please run "git fetch" command inside on this directory to update this tool.\n')
+
     except (requests.ReadTimeout,requests.ConnectTimeout, requests.ConnectionError, requests.RequestException):
         pass
 
@@ -98,14 +105,25 @@ def Check_And_Create_Settings_Yaml():
 """                     Data Reader                      """
 ############################################################
 
-with open("settings.yaml", "r") as Reader_1:
-    YML = yaml.safe_load(Reader_1)
-    Ping_List = YML["Settings"]["Ping_List"]
-    Timeout = YML["Settings"]["Default_Value"]["Timeout"]
-    Version_Compare = YML["Settings"]["Version_Control"]["Compare"]
+try:
+    with open("settings.yaml", "r") as Reader_1:
+        YML = yaml.safe_load(Reader_1)
+        Ping_List = YML["Settings"]["Ping_List"]
+        Timeout = YML["Settings"]["Default_Value"]["Timeout"]
+        Version_Compare = YML["Settings"]["Version_Control"]["Compare"]
+except FileNotFoundError:
+    with open("settings.yaml", "w") as Writter:
+        Writter.write(Settings_Template)
+        Writter.close()
+    print("[Console][Tips] Missing Required Files Added. Please Re-run The Script.")
+    exit()
 
-with open(r"../Simple_Ping/.resources/.version", "r") as Reader_2:
-    Version_Self = Reader_2.read()
+try:
+    with open(r"../Simple_Ping/.resources/.version", "r") as Reader_2:
+        Version_Self = Reader_2.read()
+except (FileNotFoundError,FileExistsError):
+    print('[Console][Warning] Missing Required Files For Checking Tool Version... Skipping.\n[Console][Tips] Please run "git fetch" command inside on this directory to update this tool.')
+    pass
 
 ############################################################
 """                    Main Function                     """
@@ -113,12 +131,19 @@ with open(r"../Simple_Ping/.resources/.version", "r") as Reader_2:
 
 def Main():
     print(Logo + Cyan + Version_Self + White)
-    Check_Tool_Version(url=Version_Compare, timeout=Timeout, version=Version_Self)
+    Check_Tool_Version(ctvurl=Version_Compare, ctvtimeout=Timeout, ctvversion=Version_Self)
     print("\n\n[Console][Prompt] Checking Your Internet Please Wait...")
-    for Item in Ping_List:
-        Ping(url=Item, timeout=Timeout)
-    print("[Console][Prompt] Session Finished!!! Exiting...")
-    exit()
+
+    while True:
+
+        for Item in Ping_List:
+            if Item is None:
+                continue
+
+            Ping(purl=Item, ptimeout=Timeout)
+
+        print("[Console][Prompt] Session Finished!!! Exiting...")
+        exit()
 
 ############################################################
 """                     Main Runner                      """
@@ -126,4 +151,10 @@ def Main():
 
 if __name__ == "__main__":
     Terminal_Cleaner()
-    Main()
+    if os.path.isfile("settings.yaml"):
+        Main()
+    else:
+        with open("settings.yaml", "w") as Writter:
+            Writter.write(Settings_Template)
+            Writter.close()
+        Main()
